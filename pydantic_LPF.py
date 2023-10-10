cea66d298f4f0d8c88872317bbe0acade824efab
Matias B <57303583+N0taR0b0t@users.noreply.github.com>
2023-10-10 17:39:39 -0400

    Added missing LPF fields

--- pydantic_LPF.py
+++ pydantic_LPF.py
@@ -1,22 +1,8 @@
 import pandas as pd
 from shapely import wkb, ops
-from pydantic import BaseModel
 import json
 import pyproj
 import warnings
-
-# Schema Representation
-class GeoJSON_T(BaseModel):
-    type: str
-    geometry: dict
-    properties: dict
-    when: dict
-
-class LinkedPlaces(BaseModel):
-    type: str
-    properties: dict
-    geometry: dict
-    when: dict
 
 # Parsing Geometry Data
 def parse_geom(hex_str):
@@ -38,20 +24,47 @@ def ingest_data(data):
 
     for index, row in data.iterrows():
         row = row.fillna("")  # replace NaN values with empty strings
-        geojson_t_item = GeoJSON_T(
-            type="Feature",
-            geometry=row['geometry'].__geo_interface__,
-            properties=row.drop('geometry').to_dict(),
-            when={"start": row['year']}
-        )
-        linked_places_item = LinkedPlaces(
-            type="Feature",
-            geometry=row['geometry'].__geo_interface__,
-            properties=row.drop('geometry').to_dict(),
-            when={"start": row['year']}
-        )
-        geojson_t_data.append(geojson_t_item.dict())
-        linked_places_data.append(linked_places_item.model_dump())  # updated method
+        
+        # Constructing the names array
+        names = []
+        if row['sname_o']:
+            names.append({"toponym": row['sname_o']})
+        if row['lname_o']:
+            names.append({"toponym": row['lname_o']})
+        if row['variants_o']:
+            names.extend([{"toponym": name.strip()} for name in row['variants_o'].split(",")])
+        if row['sname_h']:
+            names.append({"toponym": row['sname_h']})
+        if row['lname_h']:
+            names.append({"toponym": row['lname_h']})
+        if row['variants_h']:
+            names.extend([{"toponym": name.strip()} for name in row['variants_h'].split(",")])
+        
+        properties = {
+            "title": row['short_name'],
+            **row.drop(['geometry', 'geom', 'year', 'short_name', 'sname_o', 'lname_o', 'variants_o', 'sname_h', 'lname_h', 'variants_h']).to_dict()
+        }
+        
+        geojson_t_item = {
+            "@id": str(row['id']),
+            "type": "Feature",
+            "geometry": row['geometry'].__geo_interface__,
+            "properties": properties,
+            "names": names,
+            "when": {"timespans": [{"start": {"in": str(row['year'])}}]}
+        }
+        
+        linked_places_item = {
+            "@id": str(row['id']),
+            "type": "Feature",
+            "geometry": row['geometry'].__geo_interface__,
+            "properties": properties,
+            "names": names,
+            "when": {"timespans": [{"start": {"in": str(row['year'])}}]}
+        }
+        
+        geojson_t_data.append(geojson_t_item)
+        linked_places_data.append(linked_places_item)
 
     return geojson_t_data, linked_places_data
 
@@ -64,7 +77,19 @@ def main():
         data['geometry'] = data['geom'].apply(parse_geom).apply(transform_geom)  # modified this line
 
         # Data Ingestion
-        geojson_t_data, linked_places_data = ingest_data(data)
+        geojson_t_features, linked_places_features = ingest_data(data)
+
+        geojson_t_data = {
+            "@context": "http://linkedpasts.org/assets/linkedplaces-context-v1.jsonld",
+            "type": "FeatureCollection",
+            "features": geojson_t_features
+        }
+
+        linked_places_data = {
+            "@context": "http://linkedpasts.org/assets/linkedplaces-context-v1.jsonld",
+            "type": "FeatureCollection",
+            "features": linked_places_features
+        }
 
         # Output to JSON files
         with open('geojson_t_output.json', 'w') as f:
