8798ff2c7693e3653f5a80a953bfc202ac063691
Matias B <57303583+N0taR0b0t@users.noreply.github.com>
2023-10-15 17:26:51 -0400

    Removed duplicate file calculation

--- main.py
+++ main.py
@@ -4,16 +4,15 @@ import os
 def main():
     subprocess.run(['python3', 'csvDownload.py'])
     subprocess.run(['python3', 'pydantic_LPF.py'])
+    #print("Skipping python scripts")
 
     data_output_size = os.path.getsize('data_output.csv') / (1024 * 1024)  # size in MB
     linked_places_output_size = os.path.getsize('linked_places_output.json') / (1024 * 1024)  # size in MB
-    geojson_t_output_size = os.path.getsize('geojson_t_output.json') / (1024 * 1024)  # size in MB
-    total_size = (linked_places_output_size + geojson_t_output_size)
+    total_size = (linked_places_output_size)
     print()
     print(f'{data_output_size:.2f} MB turned into {total_size:.2f}')
-    print(f'data_output.csv size:	{data_output_size:.2f} MB')
-    print(f'linked_places_output.json size:	{linked_places_output_size:.2f} MB')
-    print(f'geojson_t_output.json size:	{geojson_t_output_size:.2f} MB')
+    print(f'data_output.csv size:       {data_output_size:.2f} MB')
+    print(f'linked_places_output.json size:     {linked_places_output_size:.2f} MB')
 
 if __name__ == "__main__":
     main()
